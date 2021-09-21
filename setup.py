62cf4df863473dcf1432528e0e8c6abeb383d90c
rtjohnson12 <rtjohnson1206@gmail.com>
2021-09-21 09:28:15 -0700

    :rocket: initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploration of Kaggle Agriculture dataset',
+    author='Ryan Johnson',
+    license='MIT',
+)
